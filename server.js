b2c600824df8b1f22f51f2c711520aea65adaa2c
Awal hossain <waqin21@gmail.com>
2023-01-30 22:40:13 +0600

    code changes 🌈

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = require('express')();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const mongoose = require('mongoose');
 const Document = require("./Document");
 
 
 
-mongoose.connect("mongodb+srv://documents:BAQTFVLBcI896jbe@cluster0.33slg.mongodb.net/?retryWrites=true&w=majority");
+// mongoose.connect("mongodb+srv://documents:BAQTFVLBcI896jbe@cluster0.33slg.mongodb.net/?retryWrites=true&w=majority");
 
 
-
-const io = require('socket.io')(3001,{
-    cors:{
-        origin: "*",
-        methods: ['GET', 'POST']
+const connectDb = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://documents:BAQTFVLBcI896jbe@cluster0.33slg.mongodb.net/?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('connected');
+    } catch (err) {
+        console.log('Database error');
     }
+};
+
+connectDb();
+
+// Socket
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
 });
+
 
 
 io.on("connection", (socket)=>{
@@ -49,3 +76,13 @@ async function findOrCreateDocument(id){
     return await Document.create({_id: id, data: defaultValue});
 
 }
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
